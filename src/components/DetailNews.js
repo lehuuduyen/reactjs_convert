24ff75dbe7c9668a39003141ffde3698d1e5e17b
ICDVN-DUYENLH\User <lehuuduyen>
2023-11-10 16:41:54 +0700

    update

--- src/components/DetailNews.js
+++ src/components/DetailNews.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { API_BACKEND, IMAGE_EMPTY } from "../helper/config";
 import axios from "axios";
 import { Col, Row, Skeleton, message } from "antd";
+import { Helmet } from "react-helmet-async";
 
 function DetailNews() {
   const navigate = useNavigate();
@@ -48,99 +49,132 @@ function DetailNews() {
     setDataPopular([]);
     getDetail();
   }, [navigate]);
-
+const meta = data.title
   return (
-    <Row>
-      {contextHolder}
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{meta}</title>
+        <meta name="keywords" content={meta} />
+        <meta name="description" content={data.content} />
 
-      <Col className="gutter-row" span={3}></Col>
-      <Col
-        className=" blog_area single-post-area all_post section_padding"
-        span={18}
-      >
-        <Row className="row">
-          <Col lg={16} className="Detail posts-list" style={{ marginBottom: "50px" }}>
-            <div className="single-post">
-              <div className="blog_details">
-                {Object.keys(data).length?  <h1
-                  dangerouslySetInnerHTML={{
-                    __html: data.title,
-                  }}
-                ></h1> : <><Skeleton  paragraph={{
-                  rows: 4,
-                }}></Skeleton> <Skeleton.Image active={true}  /> <Skeleton  paragraph={{
-                  rows: 20,
-                }}></Skeleton>  </>}
-               
-                <span>{data.date}</span>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: data.content,
-                  }}
-                ></div>
-              </div>
-            </div>
-          </Col>
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:title" content={meta} />
+        <meta property="og:description" content={meta} />
+        <meta property="og:image" content={data.urlToImage} />
+      </Helmet>
+      <Row>
+        {contextHolder}
 
-          <Col lg={8}>
-            <div className="sidebar_widget" style={{ paddingLeft: 20 }}>
-              <div className="single_sidebar_wiget">
-                <div className="sidebar_tittle">
-                  {dataPopular.length > 0?<h2 style={{ color: "orange" }}>Tin nổi bật</h2>:<Skeleton />} 
+        <Col className="gutter-row" span={3}></Col>
+        <Col
+          className=" blog_area single-post-area all_post section_padding"
+          span={18}
+        >
+          <Row className="row">
+            <Col
+              lg={16}
+              className="Detail posts-list"
+              style={{ marginBottom: "50px" }}
+            >
+              <div className="single-post">
+                <div className="blog_details">
+                  {Object.keys(data).length ? (
+                    <h1
+                      dangerouslySetInnerHTML={{
+                        __html: data.title,
+                      }}
+                    ></h1>
+                  ) : (
+                    <>
+                      <Skeleton
+                        paragraph={{
+                          rows: 4,
+                        }}
+                      ></Skeleton>{" "}
+                      <Skeleton.Image active={true} />{" "}
+                      <Skeleton
+                        paragraph={{
+                          rows: 20,
+                        }}
+                      ></Skeleton>{" "}
+                    </>
+                  )}
+
+                  <span>{data.date}</span>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: data.content,
+                    }}
+                  ></div>
                 </div>
-                {dataPopular.length > 0 ? (
-                  dataPopular.map((item, id) => {
-                    return (
-                      <>
-                        <div className="single_catagory_post post_2 ">
-                          <div className="category_post_img">
-                            <Link to={`/news/${item.slug}`}>
-                              <img
-                                src={
-                                  item.urlToImage
-                                    ? item.urlToImage
-                                    : IMAGE_EMPTY
-                                }
-                                alt=""
-                              />
-                            </Link>
-                          </div>
-                          <div className="post_text_1 pr_30">
-                            <Link to={`/news/${item.slug}`}>
-                              <h3
-                                dangerouslySetInnerHTML={{
-                                  __html: item.title,
-                                }}
-                              ></h3>
-                            </Link>
-                            <a
-                              href={`/news/${item.slug}`}
-                              className="a_un_underline"
-                            >
-                              <span> {item.date}</span>
-                            </a>
-                          </div>
-                        </div>
-                      </>
-                    );
-                  })
-                ) : (
-                  <>
-                    <Skeleton.Image active={true} width="100%" />
-                    <Skeleton />
-                    <Skeleton.Image active={true} width="100%" />
-                    <Skeleton />
-                    <Skeleton.Image active={true} width="100%" />
-                    <Skeleton />
-                  </>
-                )}
               </div>
-            </div>
-          </Col>
-        </Row>
-      </Col>
-      <Col className="gutter-row" span={3}></Col>
-    </Row>
+            </Col>
+
+            <Col lg={8}>
+              <div className="sidebar_widget" style={{ paddingLeft: 20 }}>
+                <div className="single_sidebar_wiget">
+                  <div className="sidebar_tittle">
+                    {dataPopular.length > 0 ? (
+                      <h2 style={{ color: "orange" }}>Tin nổi bật</h2>
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </div>
+                  {dataPopular.length > 0 ? (
+                    dataPopular.map((item, id) => {
+                      return (
+                        <>
+                          <div className="single_catagory_post post_2 ">
+                            <div className="category_post_img">
+                              <Link to={`/news/${item.slug}`}>
+                                <img
+                                  src={
+                                    item.urlToImage
+                                      ? item.urlToImage
+                                      : IMAGE_EMPTY
+                                  }
+                                  alt=""
+                                />
+                              </Link>
+                            </div>
+                            <div className="post_text_1 pr_30">
+                              <Link to={`/news/${item.slug}`}>
+                                <h3
+                                  dangerouslySetInnerHTML={{
+                                    __html: item.title,
+                                  }}
+                                ></h3>
+                              </Link>
+                              <a
+                                href={`/news/${item.slug}`}
+                                className="a_un_underline"
+                              >
+                                <span> {item.date}</span>
+                              </a>
+                            </div>
+                          </div>
+                        </>
+                      );
+                    })
+                  ) : (
+                    <>
+                      <Skeleton.Image active={true} width="100%" />
+                      <Skeleton />
+                      <Skeleton.Image active={true} width="100%" />
+                      <Skeleton />
+                      <Skeleton.Image active={true} width="100%" />
+                      <Skeleton />
+                    </>
+                  )}
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col className="gutter-row" span={3}></Col>
+      </Row>
+    </>
   );
 }
 
--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
 import { Col, Image, Row, Avatar, Divider, List, Skeleton } from "antd";
 import { API_BACKEND, IMAGE_EMPTY } from "../helper/config";
@@ -6,6 +6,7 @@ import axios from "axios";
 
 import { Link } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { Helmet } from "react-helmet-async";
 function News() {
   const [listData, setListData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -15,7 +16,7 @@ function News() {
   const appendData = () => {
     setCurrPage(currPage + 1);
     const url = API_BACKEND + `call-news?page=${currPage}`;
-     setLoading(currPage === 1);
+    setLoading(currPage === 1);
 
     axios(url, {
       mode: "no-cors",
@@ -23,10 +24,10 @@ function News() {
       credentials: "same-origin",
     })
       .then((res) => {
-        const { data, total,message, error } = res.data;
+        const { data, total, message, error } = res.data;
         if (!error) {
           setListData([...listData, ...data]);
-          setTotal(total)
+          setTotal(total);
           setLoading(false);
 
           // message.success(`${data.length} more items loaded!`);
@@ -42,137 +43,149 @@ function News() {
     appendData();
   }, []);
   // function to handle next and previous.
-
+  const meta = "Danh sách tin tức"
   //render
   return (
-    <Row>
-      {/* <Col className="gutter-row" span={3}></Col> */}
-      <Col className=" gutter-row" span={24}>
-        <div className="wrapper-24h">
-          <div
-            className="width_common list-news-subfolder o-tab__content"
-            id="_listhotnews"
-            data-campaign="Stream"
-          >
-            {loading && <Loading />}
-            {listData && !loading && (
-              <div
-                id="scrollableDiv"
-                style={{
-                  height: "calc(100vh - 105px)",
-                  overflowY: "auto",
-                  width: "100vw",
-                  // border: "1px solid rgba(140, 140, 140, 0.35)",
-                }}
-              >
-                <InfiniteScroll
-                  dataLength={listData.length}
-                  next={(e) => appendData(e)}
-                  hasMore={listData.length < total}
-                  loader={
-                    <Skeleton
-                      avatar
-                      paragraph={{
-                        rows: 1,
-                      }}
-                      active
-                    />
-                  }
-                  endMessage={
-                    <Divider plain>It is all, nothing more 🤐</Divider>
-                  }
-                  scrollableTarget="scrollableDiv"
-                >
-                  <List
-                    dataSource={listData}
-                    renderItem={(items) => (
-                      <List.Item key={items.slug}>
-                        <article
-                          key={items.slug}
-                          className="item-news item-news-common"
-                          data-offset="1"
-                        >
-                          <span className="time-count"></span>
-                          <div className="thumb-art">
-                            <Image
-                              fallback={IMAGE_EMPTY}
-                              itemprop="contentUrl"
-                              style={{
-                                transform: "translateX(-50%)",
-                                left: "50%",
-                              }}
-                              loading="lazy"
-                              intrinsicsize="220x132"
-                              alt={items.title}
-                              className="lazy lazied thumb "
-                              src={items.urlToImage}
-                              data-ll-status="loaded"
-                            />
-                          </div>
-                          <h3 className="title-news">
-                            <Link
-                              data-medium="Item-1"
-                              data-thumb="1"
-                              to={`/news/${items.slug}`}
-                              title={items.title}
-                              data-itm-source="#vn_source=TinNong&amp;vn_campaign=Stream&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1"
-                              data-itm-added="1"
-                            >
-                              <span
-                                dangerouslySetInnerHTML={{
-                                  __html: items.title,
-                                }}
-                              ></span>
-                            </Link>
-                          </h3>
-                          <p className="description">
-                            <Link
-                              className="a_un_underline"
-                              data-medium="Item-1"
-                              data-thumb="1"
-                              to={`/news/${items.slug}`}
-                              title={items.title}
-                              data-itm-source="#vn_source=TinNong&amp;vn_campaign=Stream&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1"
-                              data-itm-added="1"
-                            >
-                              <span>{items.content}</span>
-                            </Link>
-                          </p>
-                          <p className="description">
-                            <Link
-                              className="a_un_underline"
-                              data-medium="Item-1"
-                              data-thumb="1"
-                              to={`/news/${items.slug}`}
-                              title={items.title}
-                              data-itm-source="#vn_source=TinNong&amp;vn_campaign=Stream&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1"
-                              data-itm-added="1"
-                            >
-                              <p className="meta-news">
-                                <span className="time-public">
-                                  <span
-                                    datetime="2023-10-17 14:06:06"
-                                    timeago-id="240"
-                                  >
-                                    {items.date}
-                                  </span>
-                                </span>
-                              </p>
-                            </Link>
-                          </p>
-                        </article>
-                      </List.Item>
-                    )}
-                  />
-                </InfiniteScroll>
-              </div>
-            )}
-          </div>
-        </div>
-      </Col>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{meta}</title>
+        <meta name="keywords" content={meta} />
+        <meta name="description" content={meta} />
 
-      {/* <Col className="gutter-row" span={3}></Col> */}
-    </Row>
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:title" content={meta} />
+        <meta property="og:description" content={meta} />
+      </Helmet>
+      <Row>
+        {/* <Col className="gutter-row" span={3}></Col> */}
+        <Col className=" gutter-row" span={24}>
+          <div className="wrapper-24h">
+            <div
+              className="width_common list-news-subfolder o-tab__content"
+              id="_listhotnews"
+              data-campaign="Stream"
+            >
+              {loading && <Loading />}
+              {listData && !loading && (
+                <div
+                  id="scrollableDiv"
+                  style={{
+                    height: "calc(100vh - 105px)",
+                    overflowY: "auto",
+                    width: "100vw",
+                    // border: "1px solid rgba(140, 140, 140, 0.35)",
+                  }}
+                >
+                  <InfiniteScroll
+                    dataLength={listData.length}
+                    next={(e) => appendData(e)}
+                    hasMore={listData.length < total}
+                    loader={
+                      <Skeleton
+                        avatar
+                        paragraph={{
+                          rows: 1,
+                        }}
+                        active
+                      />
+                    }
+                    endMessage={
+                      <Divider plain>It is all, nothing more 🤐</Divider>
+                    }
+                    scrollableTarget="scrollableDiv"
+                  >
+                    <List
+                      dataSource={listData}
+                      renderItem={(items) => (
+                        <List.Item key={items.slug}>
+                          <article
+                            key={items.slug}
+                            className="item-news item-news-common"
+                            data-offset="1"
+                          >
+                            <span className="time-count"></span>
+                            <div className="thumb-art">
+                              <Image
+                                fallback={IMAGE_EMPTY}
+                                itemprop="contentUrl"
+                                style={{
+                                  transform: "translateX(-50%)",
+                                  left: "50%",
+                                }}
+                                loading="lazy"
+                                intrinsicsize="220x132"
+                                alt={items.title}
+                                className="lazy lazied thumb "
+                                src={items.urlToImage}
+                                data-ll-status="loaded"
+                              />
+                            </div>
+                            <h3 className="title-news">
+                              <Link
+                                data-medium="Item-1"
+                                data-thumb="1"
+                                to={`/news/${items.slug}`}
+                                title={items.title}
+                                data-itm-source="#vn_source=TinNong&amp;vn_campaign=Stream&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1"
+                                data-itm-added="1"
+                              >
+                                <span
+                                  dangerouslySetInnerHTML={{
+                                    __html: items.title,
+                                  }}
+                                ></span>
+                              </Link>
+                            </h3>
+                            <p className="description">
+                              <Link
+                                className="a_un_underline"
+                                data-medium="Item-1"
+                                data-thumb="1"
+                                to={`/news/${items.slug}`}
+                                title={items.title}
+                                data-itm-source="#vn_source=TinNong&amp;vn_campaign=Stream&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1"
+                                data-itm-added="1"
+                              >
+                                <span>{items.content}</span>
+                              </Link>
+                            </p>
+                            <p className="description">
+                              <Link
+                                className="a_un_underline"
+                                data-medium="Item-1"
+                                data-thumb="1"
+                                to={`/news/${items.slug}`}
+                                title={items.title}
+                                data-itm-source="#vn_source=TinNong&amp;vn_campaign=Stream&amp;vn_medium=Item-1&amp;vn_term=Desktop&amp;vn_thumb=1"
+                                data-itm-added="1"
+                              >
+                                <p className="meta-news">
+                                  <span className="time-public">
+                                    <span
+                                      datetime="2023-10-17 14:06:06"
+                                      timeago-id="240"
+                                    >
+                                      {items.date}
+                                    </span>
+                                  </span>
+                                </p>
+                              </Link>
+                            </p>
+                          </article>
+                        </List.Item>
+                      )}
+                    />
+                  </InfiniteScroll>
+                </div>
+              )}
+            </div>
+          </div>
+        </Col>
+
+        {/* <Col className="gutter-row" span={3}></Col> */}
+      </Row>
+    </>
   );
 }
 
